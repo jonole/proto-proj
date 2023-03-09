15845f75195d9b80edef828ade34adc896243701
Jon Ole Segerfeldt <joueland@gmail.com>
2023-03-09 09:01:11 +0100

    updates gulpfile with new compilers

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,53 @@
-let gulp = require('gulp');
-let autoprefixer = require('gulp-autoprefixer');
-let sass = require('gulp-sass');
-let cleanCSS = require('gulp-clean-css');
-let browserSync = require('browser-sync').create();
-var babel = require('gulp-babel');
+const gulp = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
 
-// Compile sass into CSS, autoprefix & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("css/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({ grid: true, browsers: ['>1%'] }))
-        .pipe(gulp.dest("css"))
-        .pipe(browserSync.stream());
-});
+const paths = {
+  styles: {
+    src: ['./sass/**/*.scss'],
+    dist: './css',
+  },
+  scripts: {
+    src: ['./js/**/*.js'],
+    dist: './dist',
+  }
+};
 
-// Minify css-files
-gulp.task('minify-css', function(){
-  return gulp.src('css/*.css')
-    .pipe(cleanCSS({compatability: 'ie9'}))
-    .pipe(gulp.dest('css'))
-});
+function compileStyles() {
+  return src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({ grid: true } ))
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(paths.styles.dist));
+}
+function compileScripts() {
+  return src(paths.scripts.src)
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))    
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(dest(paths.scripts.dist));
+}
 
-gulp.task('babel', function(){
-	return gulp.src('js/*.js')
-		.pipe(babel({
-			presets: ['@babel/env']
-		}))
-		.pipe(gulp.dest('dist'))
-});
+function watcher() {
+  watch(paths.styles.src, compileStyles);
+  watch(paths.scripts.src, compileScripts);
+  browserSync.reload
+}
 
-// Static Server + watching scss/html files
-gulp.task('serve', function() {
+exports.compileStyles = compileStyles;
+exports.babelScripts = compileScripts;
 
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("sass/*.scss", gulp.series(['sass', 'minify-css']));
-    gulp.watch('js/*.js', gulp.series(['babel']));
-    gulp.watch("*.html").on('change', browserSync.reload);
-});
-
-
-gulp.task('default', gulp.series('serve'));
+exports.default = series(
+  parallel(compileStyles, compileScripts),
+  watcher
+);
